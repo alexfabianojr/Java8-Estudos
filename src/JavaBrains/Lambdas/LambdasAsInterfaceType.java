2144e9169d5af8ba8a9fa92877c897e7cc772cf6
alex.baptista <alex.baptista@dbccompany.com.br>
2020-10-21 02:04:03 -0300

    LambdasAsInterfaceType

---
+++ src/JavaBrains/Lambdas/LambdasAsInterfaceType.java
@@ -0,0 +1,19 @@
+package JavaBrains.Lambdas;
+
+public class LambdasAsInterfaceType {
+
+    public static void main(String[] args) {
+
+        MyLambda myLambdaFunction = ( ) -> System.out.println("Hello World!");
+
+        MyAdd myAddFunction = ( x, y ) -> x + y;
+    }
+}
+
+interface MyLambda {
+    void foo();
+}
+
+interface MyAdd {
+    int add(int x, int y);
+}
